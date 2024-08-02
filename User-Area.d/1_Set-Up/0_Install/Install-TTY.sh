8f069ffaed762efbfa756bbb01143a52eb3a3dec
ohSystemmm <leonhardfresacher@gmail.com>
2024-08-02 21:30:09 +0200

    Idk lol

--- User-Area.d/1_Set-Up/0_Install/Install-TTY.sh
+++ User-Area.d/1_Set-Up/0_Install/Install-TTY.sh
@@ -10,3 +10,56 @@ source include/Assets.sh
 
 SlideHeader "Warning!"
 NextSlide
+
+SlideHeader "Prerequisite Packages."
+NextSlide
+
+SlideHeader "Welcome!"
+NextSlide
+
+SlideHeader "Licence."
+NextSlide
+
+SlideHeader ""
+
+SlideHeader "Verifying Compatibility."
+NextSlide
+
+SlideHeader "Folder Location."
+NextSlide
+
+SlideHeader "pacman."
+NextSlide
+
+SlideHeader "yay."
+NextSlide
+
+SlideHeader "paru."
+NextSlide
+
+SlideHeader "Required Packages."
+NextSlide
+
+SlideHeader "Optional Packages."
+NextSlide
+
+SlideHeader "ohSystemmm Packages."
+NextSlide
+
+SlideHeader "Enabeling sddm."
+NextSlide
+
+SlideHeader "Enabeling tlp." # install tlp
+NextSlide
+
+SlideHeader "Customizing grub."
+NextSlide
+
+SlideHeader "Linking Apps."
+NextSlide
+
+SlideHeader "Clean Up."
+NextSlide
+
+SlideHeader "Done!"
+NextSlide
