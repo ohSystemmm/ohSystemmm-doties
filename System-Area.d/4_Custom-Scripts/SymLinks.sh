4b60674dc431dc2c5beda29a172a539bafc4ca0b
ohSystemmm <leonhardfresacher@gmail.com>
2024-08-23 11:11:25 +0200

    Added WORKING symlinks

--- System-Area.d/4_Custom-Scripts/SymLinks.sh
+++ System-Area.d/4_Custom-Scripts/SymLinks.sh
@@ -7,4 +7,17 @@
 # 
 # by ohSystemmm <3 - 2024
 
+ln -s ~/ohSystemmm-doties/System-Area.d/3_Package-Config/0_App-Configs/ags/  ~/.config/
 ln -s ~/ohSystemmm-doties/System-Area.d/3_Package-Config/0_App-Configs/btop/ ~/.config/ 
+ln -s ~/ohSystemmm-doties/System-Area.d/3_Package-Config/0_App-Configs/cava/ ~/.config/ 
+ln -s ~/ohSystemmm-doties/System-Area.d/3_Package-Config/0_App-Configs/fastfetch/ ~/.config/ 
+ln -s ~/ohSystemmm-doties/System-Area.d/3_Package-Config/0_App-Configs/gtk-3.0/ ~/.config/ 
+ln -s ~/ohSystemmm-doties/System-Area.d/3_Package-Config/0_App-Configs/picom/ ~/.config/ 
+ln -s ~/ohSystemmm-doties/System-Area.d/3_Package-Config/0_App-Configs/rofi/ ~/.config/ 
+ln -s ~/ohSystemmm-doties/System-Area.d/3_Package-Config/0_App-Configs/superfile/ ~/.config/ 
+ln -s ~/ohSystemmm-doties/System-Area.d/3_Package-Config/0_App-Configs/wlogout/ ~/.config/ 
+ln -s ~/ohSystemmm-doties/System-Area.d/3_Package-Config/1_Editor-Configs/nvim/ ~/.config/ 
+ln -s ~/ohSystemmm-doties/System-Area.d/3_Package-Config/3_Terminal-Configs/kitty/ ~/.config/ 
+ln -s ~/ohSystemmm-doties/System-Area.d/3_Package-Config/3_Terminal-Configs/wezterm/ ~/.config/
+ln -s ~/ohSystemmm-doties/System-Area.d/2_Hyprland-Config/hypr/ ~/.config/ 
+
