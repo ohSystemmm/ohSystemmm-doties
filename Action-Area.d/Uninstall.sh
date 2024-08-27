e1563a02e74c70ee33fb74cce7488a75b2db02e3
ohSystemmm <leonhardfresacher@gmail.com>
2024-08-27 11:25:02 +0200

    Working Deltion Script

--- Action-Area.d/Uninstall.sh
+++ Action-Area.d/Uninstall.sh
@@ -5,3 +5,25 @@
 #  \___/|_| |_|_|_| |_|___/\__\__,_|_|_(_)___/_| |_|
 #                          
 # by ohSystemmm <3 - 2024
+
+source assets/Assets.sh
+
+SlideHeader "Uninstall"
+
+if gum confirm "Are you sure to Uninstall?" --affirmative "Confirm" --negative "Exit"; then
+  echo -e "Starting ..."
+  echo -e "\nRemoving Bootassets."
+  sudo cp -f ../System-Area.d/6_Defaults/grub /etc/default/grub
+  sudo rm -rf /boot/grub/themes/Grub/ 
+  sudo grub-mkconfig -o /boot/grub/grub.cfg
+  echo -e "\nRemoving Sddmassets."
+  sudo rm -rf /etc/sddm.conf
+  sudo rm -rf /usr/share/sddm/themes/Sddm-Theme/
+  echo -e "\nRemoving Dotfiles."
+  sudo rm -rf ~/ohSystemmm-doties/ 
+  echo "Done."
+  reboot
+else
+  echo -e "Canceled.\n"
+  exit 1
+fi
