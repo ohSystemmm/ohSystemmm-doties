776f88d24f7d2e7e5cb0040e71e12bde82d27c2a
ohSystemmm <leonhardfresacher@gmail.com>
2024-09-03 16:16:39 +0200

    Corny Symlinks

--- System-Area.d/4_Custom-Scripts/SymLinks.sh
+++ System-Area.d/4_Custom-Scripts/SymLinks.sh
@@ -8,46 +8,61 @@
 # by ohSystemmm <3 - 2024
 
 # ags -------------------------------------------------------------------------------------------
+rm -rf ~/.config/ags/
 ln -s ~/ohSystemmm-doties/System-Area.d/3_Package-Config/0_App-Configs/ags/  ~/.config/
 
 # btop ------------------------------------------------------------------------------------------
+rm -rf ~/.config/btop/
 ln -s ~/ohSystemmm-doties/System-Area.d/3_Package-Config/0_App-Configs/btop/ ~/.config/ 
 
 # cava ------------------------------------------------------------------------------------------
+rm -rf ~/.config/cava/
 ln -s ~/ohSystemmm-doties/System-Area.d/3_Package-Config/0_App-Configs/cava/ ~/.config/ 
 
 # fastfetch -------------------------------------------------------------------------------------
+rm -rf ~/.config/fastfetch/
 ln -s ~/ohSystemmm-doties/System-Area.d/3_Package-Config/0_App-Configs/fastfetch/ ~/.config/ 
 
 # gtk-3.0 ---------------------------------------------------------------------------------------
+rm -rf ~/.config/gtk-3.0/
 ln -s ~/ohSystemmm-doties/System-Area.d/3_Package-Config/0_App-Configs/gtk-3.0/ ~/.config/ 
 
 # picom -----------------------------------------------------------------------------------------
+rm -rf ~/.config/picom/
 ln -s ~/ohSystemmm-doties/System-Area.d/3_Package-Config/0_App-Configs/picom/ ~/.config/ 
 
 # rofi ------------------------------------------------------------------------------------------
+rm -rf ~/.config/rofi/
 ln -s ~/ohSystemmm-doties/System-Area.d/3_Package-Config/0_App-Configs/rofi/ ~/.config/ 
 
 # superfile -------------------------------------------------------------------------------------
+rm -rf ~/.config/superfile/
 ln -s ~/ohSystemmm-doties/System-Area.d/3_Package-Config/0_App-Configs/superfile/ ~/.config/ 
 
 # wlogout ---------------------------------------------------------------------------------------
+rm -rf ~/.config/wlogout/
 ln -s ~/ohSystemmm-doties/System-Area.d/3_Package-Config/0_App-Configs/wlogout/ ~/.config/ 
 
 # nvim ------------------------------------------------------------------------------------------
+rm -rf ~/.config/nvim/
 ln -s ~/ohSystemmm-doties/System-Area.d/3_Package-Config/1_Editor-Configs/nvim/ ~/.config/ 
 
 # kitty -----------------------------------------------------------------------------------------
+rm -rf ~/.config/kitty/
 ln -s ~/ohSystemmm-doties/System-Area.d/3_Package-Config/3_Terminal-Configs/kitty/ ~/.config/ 
 
 # wezterm ---------------------------------------------------------------------------------------
+rm -rf ~/.config/wezterm/
 ln -s ~/ohSystemmm-doties/System-Area.d/3_Package-Config/3_Terminal-Configs/wezterm/ ~/.config/
 
 # hypr ------------------------------------------------------------------------------------------
+rm -rf ~/.config/hypr/
 ln -s ~/ohSystemmm-doties/System-Area.d/2_Hyprland-Config/hypr/ ~/.config/ 
 
 # zsh -------------------------------------------------------------------------------------------
+rm -rf ~/.config/.zshrc
 ln -s ~/ohSystemmm-doties/System-Area.d/3_Package-Config/2_Shell-Configs/zsh/.zshrc ~/.zshrc
 
 # fish ------------------------------------------------------------------------------------------
+rm -rf ~/.config/fish/
 ln -s ~/ohSystemmm-doties/System-Area.d/3_Package-Config/2_Shell-Configs/fish/ ~/.config/
