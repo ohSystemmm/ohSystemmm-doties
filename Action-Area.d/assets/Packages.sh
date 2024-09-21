b26cf9bf63fca40ee017afb8cd41795e9744a71f
システム君 <leonhardfresacher@gmail.com>
2024-09-21 11:27:09 +0200

    Slack

--- Action-Area.d/assets/Packages.sh
+++ Action-Area.d/assets/Packages.sh
@@ -131,6 +131,7 @@ InstallohSystemmm() {
   # qemu                               # A basic QEMU setup for headless environments
     rider                              # A cross-platform .NET IDE by JetBrains.
     signal-desktop                     # Signal Private Messenger for Linux
+    slack-desktop-wayland              # Slack Desktop (Beta) for Linux with Wayland Support
     speedtest-cli                      # Command line interface for testing internet bandwidth using speedtest.net
     spotify                            # A proprietary music streaming service
     superfile-bin                      # Pretty fancy and modern file manager in terminal file manager
