dbb5e66fba242ee6f8e6189fbadda3a8fa6d28c1
システム君 <leonhardfresacher@gmail.com>
2025-03-22 13:47:26 +0100

    Removed prism

--- System-Area.d/4_Custom-Scripts/SymLinks.sh
+++ System-Area.d/4_Custom-Scripts/SymLinks.sh
@@ -67,6 +67,3 @@ ln -s ~/ohSystemmm-doties/System-Area.d/3_Package-Config/2_Shell-Configs/zsh/.zs
 rm -rf ~/.config/fish/
 ln -s ~/ohSystemmm-doties/System-Area.d/3_Package-Config/2_Shell-Configs/fish/ ~/.config/
 
-# PrismLauncher ----------------------------------------------------------------------------------
-rm -rf ~/.local/share/PrismLauncher/ 
-ln -s ~/ohSystemmm-doties/System-Area.d/3_Package-Config/0_App-Configs/PrismLauncher/ ~/.local/share/PrismLauncher  
